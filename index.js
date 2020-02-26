48972375dce0b31b8454616504d6f515cf722665
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-02-26 16:36:53 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+class Scooter {
+    constructor(year, color, model){
+        this.year = year
+        this.color = color
+        this.model = model
+    }
+}
+    let x = new Scooter("2020", "red", "v8")
+
+
+class Driver {
+    constructor (name, age, experience){
+        this.name = name
+        this.age = age
+        this.experience = experience
+    }
+}
+    let y = new Driver("", "2", "")
+
+class PickupLocation {
+    constructor (address, city) {
+        this.address = address
+        this.city = city
+    }
+}
+    let z = new PickupLocation()
