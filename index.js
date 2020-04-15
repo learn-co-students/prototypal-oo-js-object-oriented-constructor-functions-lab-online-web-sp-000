8244a03675c3e0f7c51f72208c8fbb73517bd2c9
Malcolm Leake <echelon1089@gmail.com>
2020-04-15 22:16:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+class Scooter {
+  constructor(year, color, model){
+    this.year = year
+    this.color = color
+    this.model = model
+  }
+}
+
+class Driver {
+  constructor(name, age, experience){
+    this.name = name
+    this.age = age
+    this.experience = experience
+  }
+}
+
+class PickupLocation {
+  constructor(adress, city){
+    this.address = adress
+    this.city = city
+  }
+}
